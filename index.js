db7a875a567a66d386df03919de0cb3460e90f60
EnzoDel <137450110+EnzoDel@users.noreply.github.com>
2023-07-08 12:43:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ config()
 const app = express()
 
 const pool = new pg.Pool({
-   connectionString: process.env.DATABASE_URL,
-   ssl:true
+   connectionString: process.env.DATABASE_URL
+   
 })
 
 app.get('/',(req,res)=>{
